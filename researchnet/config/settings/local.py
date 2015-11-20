d2e08ef42ff936b53057cbbb0860a0631119eff3
Adam Preston <adam.m.preston@gmail.com>
2015-11-20 09:58:11 -0700

    call the local db one thing

--- researchnet/config/settings/local.py
+++ researchnet/config/settings/local.py
@@ -10,7 +10,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': 'db.local',
         'PORT': 5432,
     }
 }
