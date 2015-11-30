75ab741d21560c12ab60b91e0f07743856e9a0bf
Adam Preston <adam.m.preston@gmail.com>
2015-11-30 15:58:33 -0700

    port mapping should be 80 on host

--- deployment/docker-entrypoint.sh
+++ deployment/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo Doing Routine Maintenance.
 python manage.py migrate                  # Apply database migrations
 python manage.py collectstatic --noinput  # Collect static files
 python manage.py bower install			  # install frontend assets
