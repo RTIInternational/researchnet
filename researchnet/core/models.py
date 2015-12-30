08be7d31b994981d2d64e15067feac2dafd0ec0d
Adam Preston <adam.m.preston@gmail.com>
2015-12-29 18:23:19 -0700

    use signal to create token for new users

--- researchnet/core/models.py
+++ researchnet/core/models.py
@@ -3,8 +3,9 @@ from django.conf import settings
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 from django.contrib.auth.models import User
-
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class Submission(models.Model):
@@ -27,4 +28,7 @@ class Consent(models.Model):
     imageData = models.ImageField(blank=True)
     consent_date = models.DateTimeField(default=datetime.now)
 
-
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
