5c83bb956642b78f09a2b91378e62770711693fd
Adam Preston <adam.m.preston@gmail.com>
2016-01-02 12:46:37 -0700

    upped gunicorn timeout (again)

--- deployment/docker-entrypoint.sh
+++ deployment/docker-entrypoint.sh
@@ -10,6 +10,6 @@ exec gunicorn config.wsgi:application \
     --name researchnet \
     --bind 0.0.0.0:8000 \
     --workers 3 \
-    --timeout 5 \
+    --timeout 30 \
 	--log-level=debug \
     "$@"
