76ed0e8d42e93ccc0a9be3d2ec63833e10da6d5b
Adam Preston <adam.m.preston@gmail.com>
2016-01-03 13:26:45 -0700

    production should probably have a database configured

--- researchnet/config/settings/production.py
+++ researchnet/config/settings/production.py
@@ -1,6 +1,16 @@
 from .base import *
 
 # Obviously this shouldn't be true but until the web proxy is setup (and nginx serves up the static files) we have to run the app in debug mode.
+
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
 
--- researchnet/config/settings/test.py
+++ researchnet/config/settings/test.py
@@ -1,1 +1,12 @@
 from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
