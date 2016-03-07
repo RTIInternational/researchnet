02963eb488205419925dde7b0372d81ae05a427f
Adam Preston <adam.m.preston@gmail.com>
2016-03-07 11:30:53 -0700

    query submission list for dashboard Added notebook example for querying database

--- researchnet/dashboard/views.py
+++ researchnet/dashboard/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from core.models import Submission
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'index.html')
+    submissions = Submission.objects.order_by('timestamp')[:10]
+    context = {'submission_list': submissions}
+    return render(request, 'index.html', context)
+
