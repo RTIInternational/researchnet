0dd630ed35d19b9abb4f329b11ba608110646f3d
Adam Preston <adam.m.preston@gmail.com>
2016-03-24 15:06:51 -0600

    Don't debug production
    its the least you can do

--- researchnet/config/settings/production.py
+++ researchnet/config/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 # Obviously this shouldn't be true but until the web proxy is setup (and nginx serves up the static files) we have to run the app in debug mode.
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
