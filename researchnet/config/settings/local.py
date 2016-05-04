b20c59db79a44b4c59bff30b379d0bff21fed660
Adam Preston <adam.m.preston@gmail.com>
2016-05-04 11:02:40 -0600

    make it clear that things will go wrong later if the app isn't configured right

--- researchnet/config/settings/local.py
+++ researchnet/config/settings/local.py
@@ -11,3 +11,10 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+# there will be problems later if these aren't set
+assert 'EMAIL_HOST' in os.environ, 'Set EMAIL_HOST in your settings!'
+assert 'EMAIL_HOST_USER' in os.environ, 'Set EMAIL_HOST_USER in your settings!'
+assert 'EMAIL_HOST_PASSWORD' in os.environ, 'Set EMAIL_HOST_PASSWORD in your settings!'
+assert 'EMAIL_PORT' in os.environ, 'Set EMAIL_PORT in your settings!'
+assert 'EMAIL_USE_TLS' in os.environ, 'Set EMAIL_USE_TLS in your settings!'
